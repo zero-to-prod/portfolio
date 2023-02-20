5cc0fe36e56051bfba93dca794aa06ddcd178e03
David Smith <dasmith@zero-to-prod.com>
2023-02-19 19:23:52 -0500

    Add addition helper functions

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -16,6 +16,14 @@ if (!function_exists('route_name_is')) {
     }
 }
 
+if (!function_exists('route_name_isnt')) {
+
+    function route_name_isnt($route): bool
+    {
+        return !route_name_is($route);
+    }
+}
+
 if (!function_exists('route_is')) {
 
     function route_is($route): bool
@@ -25,5 +33,13 @@ if (!function_exists('route_is')) {
         }
 
         return Route::current()->uri === $route;
+    }
+}
+
+if (!function_exists('route_isnt')) {
+
+    function route_isnt($route): bool
+    {
+        return !route_is($route);
     }
 }

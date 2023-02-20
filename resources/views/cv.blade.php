1a2bb6293571c5518d011c5094e27d25e60e9b2c
David Smith <dasmith@zero-to-prod.com>
2023-02-20 09:52:51 -0500

    Updated cv.blade.php

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -3,19 +3,36 @@
         <ol role="list" class="overflow-hidden">
             <li class="relative pb-10">
                 <div class="absolute top-12 left-12 -ml-px mt-0.5 h-full w-0.5 bg-sky-600" aria-hidden="true"></div>
-                <a href="#" class="group relative flex items-start">
+                <div class="group relative flex items-start">
                   <span class="flex m-2 items-center shadow-lg">
                     <span
                         class="relative z-10 flex h-20 w-20 items-center justify-center">
                       <img src="{{ Vite::asset('resources/images/viewrail.jpg') }}" alt="viewrail logo"/>
                     </span>
                   </span>
-                    <span class="ml-4 flex min-w-0 flex-col mt-6">
+                    <div class="ml-4 flex min-w-0 flex-col mt-6">
                         <span class="text-sm font-medium">Senior Full Stack Developer</span>
                         <span class="text-sm text-gray-800">ViewRail Full-time </span>
                         <span class="text-sm text-gray-500">Jan 2022 - Feb 2023 · 1 yr 2 mos</span>
-                  </span>
-                </a>
+                        <details open class="text-sm text-gray-800 ">
+                            <summary class="font-medium cursor-pointer py-2">Details</summary>
+                            <p class="mb-2"> Responsibilities: </p>
+                            <ul class="list-disc ml-4 ">
+                                <li>Led the architecting and implementing of the companies legacy ERP system into a
+                                    modern technology stack
+                                </li>
+                                <li>Worked with the many teams and stakeholders of the company to move features into the
+                                    new system
+                                </li>
+                                <li>Implemented automated testing and improved the deployment pipeline</li>
+                                <li>Simplified the database through schema changes and data migrations</li>
+                                <li>Was the primary person responsible to keep the systems running and available</li>
+                                <li>Was responsible for code reviews, training, collaboration .etc</li>
+                            </ul>
+                            <p class="mb-2"> Responsibilities: </p>
+                        </details>
+                    </div>
+                </div>
             </li>
 
             <li class="relative pb-10">
@@ -33,6 +50,61 @@
                         <span class="text-sm text-gray-500">Apr 2021 - Jan 2022 · 10 mos</span>
                   </span>
                 </a>
+            </li>
+
+            <li class="relative pb-10">
+                <div class="absolute top-12 left-12 -ml-px mt-0.5 h-full w-0.5 bg-sky-600" aria-hidden="true"></div>
+                <div class="group relative flex items-start">
+                  <span class="flex m-2 items-center shadow-lg">
+                    <span
+                        class="relative z-10 flex h-20 w-20 items-center justify-center">
+                      <img src="{{ Vite::asset('resources/images/viewrail.jpg') }}" alt="viewrail logo"/>
+                    </span>
+                  </span>
+                    <div class="ml-4 flex min-w-0 flex-col mt-6">
+                        <span class="text-sm font-medium">Technologies I used while at ViewRail</span>
+                        <span class="text-sm text-gray-800">Docker, React/TS, Laravel, Graphql</span>
+                        <span class="text-sm text-gray-500">Apr 2021 - Feb 2023 · ~2 years</span>
+                        <details open class="text-sm text-gray-800 ">
+                            <summary class="font-medium cursor-pointer py-2">Details</summary>
+                            <p class="mb-2"> Technologies I used while at ViewRail </p>
+                            <p class="mb-2 font-medium"> Frontend: </p>
+                            <ul class="list-disc ml-4 ">
+                                <li>React (functional components)</li>
+                                <li>TypeScript</li>
+                                <li>MATERIAL-UI</li>
+                                <li>Formik</li>
+                                <li>GraphQL</li>
+                                <li>Apollo-GraphQL</li>
+                                <li>GraphQL-Codegen</li>
+                                <li>create-react-app</li>
+                                <li>Recharts</li>
+                                <li>Moment.js</li>
+                                <li>Prettier</li>
+                                <li>NodeJs</li>
+                                <li>BABEL</li>
+                                <li>npm</li>
+                            </ul>
+                            <p class="mb-2 font-medium"> Frontend: </p>
+                            <ul class="list-disc ml-4 ">
+                                <li>PHP</li>
+                                <li>Laravel</li>
+                                <li>Passport</li>
+                                <li>Lighthouse-php</li>
+                                <li>GraphQL</li>
+                                <li>Apollo-GraphQL</li>
+                                <li>GraphQL-Codegen</li>
+                                <li>create-react-app</li>
+                                <li>Recharts</li>
+                                <li>Moment.js</li>
+                                <li>Prettier</li>
+                                <li>NodeJs</li>
+                                <li>BABEL</li>
+                                <li>npm</li>
+                            </ul>
+                        </details>
+                    </div>
+                </div>
             </li>
 
             <li class="relative pb-10">

b229c98c41f9effa09d0d0e756d208cb1f84765b
David Smith <dasmith@zero-to-prod.com>
2023-02-20 17:14:21 -0500

    Remove `resources/js/app.js` in main.blade.php Add `resources/js/top_nav_scroll.js` in vite.config.js

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="David Smith Blog">
     <title>davidDESIGN</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css'])
 </head>
 <body>
 <x-top-nav/>

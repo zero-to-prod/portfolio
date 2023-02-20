8276571eaf9b627b1be21eaa614c0349f76378d1
David Smith <dasmith@zero-to-prod.com>
2023-02-20 17:21:26 -0500

    Remove `resources/js/app.js` in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/top_nav_scroll.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/top_nav_scroll.js'
+            ],
             refresh: true,
         }),
     ],

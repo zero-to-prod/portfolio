3df8698b310634323dbea0442e77997882b077b9
David Smith <dasmith@zero-to-prod.com>
2023-02-21 10:49:03 -0500

    Add Message model

---
+++ app/Models/Message.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Support\IdColumn;
+use App\Models\Support\MessageColumns;
+use App\Models\Support\SoftDeleteColumn;
+use App\Models\Support\TimeStampColumns;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Message extends Model
+{
+    use HasFactory;
+    use IdColumn;
+    use TimeStampColumns;
+    use SoftDeletes;
+    use SoftDeleteColumn;
+    use MessageColumns;
+
+}

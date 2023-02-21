5e2000f85bcf9a889d0fd17a393886de84249a6f
David Smith <dasmith@zero-to-prod.com>
2023-02-20 19:26:30 -0500

    Update `top-nav.blade.php`

--- resources/views/components/top-nav.blade.php
+++ resources/views/components/top-nav.blade.php
@@ -8,17 +8,17 @@
     <div class="mx-auto max-w-7xl px-4">
         <div class="flex h-16 justify-between">
             <div class="flex">
-                <div class="flex space-x-8">
+                <div class="flex">
                     <a href="{{Routes::welcome->value}}"
                         @class([
-                            'inline-flex items-center border-b-2 px-1 pt-1 text-lg',
+                            'inline-flex items-center border-b-2 sm:text-lg',
                             'border-sky-500 text-gray-900' => route_name_is(Routes::welcome),
                             'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' => route_name_isnt(Routes::welcome),
                             ])
                     >david<span class="text-sky-500">DESIGN</span></a>
                     <a href="{{Routes::cv->value}}"
                         @class([
-                            'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium',
+                            'inline-flex items-center border-b-2 text-sm font-medium px-2 sm:px-4',
                             'border-sky-500 text-gray-900' => route_name_is(Routes::cv),
                             'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700' => route_name_isnt(Routes::cv),
                             ])

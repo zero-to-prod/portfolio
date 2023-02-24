2141bf9518eed9f1869a94b93581fb9b2bf30a99
David Smith <dasmith@zero-to-prod.com>
2023-02-24 14:40:27 -0500

    Refactor route `email`

--- app/Http/Routes.php
+++ app/Http/Routes.php
@@ -4,20 +4,22 @@ namespace App\Http;
 
 enum Routes: string
 {
-    case cv = '/cv';
     case connect = '/connect';
     case connect_store = '/connect/store';
+    case cv = '/cv';
     case welcome = '/';
-    case dashboard = '/dashboard';
 
-    case profile_edit = '/profile/edit';
-    case profile_update = '/profile/update';
-    case profile_destroy = '/profile/destroy';
-    case logout = 'logout';
+    /* Admin */
+
+    case dashboard = '/dashboard';
     case email_verificationNotice = 'email/verification-notice';
     case email_verificationNotification = 'email/verification-notification';
     case email_verify = 'email/verify/{id}/{hash}';
+    case logout = 'logout';
     case password_confirm = 'password/confirm';
     case password_store = 'password/store';
     case password_update = 'password/update';
+    case profile_destroy = '/profile/destroy';
+    case profile_edit = '/profile/edit';
+    case profile_update = '/profile/update';
 }

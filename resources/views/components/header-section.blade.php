921430473314dac88bd1b9a1b3c8cdf528aebcfa
David Smith <dasmith@zero-to-prod.com>
2023-02-24 07:18:40 -0500

    Update Welcome page.

--- resources/views/components/header-section.blade.php
+++ resources/views/components/header-section.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-gradient-to-b from-sky-200 to-white py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto max-w-2xl" data-header-section="header" >
+        <div class="mx-auto max-w-3xl" data-header-section="header" >
             {{$slot}}
         </div>
     </div>

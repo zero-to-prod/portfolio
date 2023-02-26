10dfd85368936700a82f58b5e58b019eb8085540
David Smith <dasmith@zero-to-prod.com>
2023-02-26 08:59:04 -0500

    Refactor to `route_is()`

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,7 +19,7 @@ use App\Http\Routes;
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route_as(Routes::dashboard)"
-                                :active="request()->routeIs(Routes::dashboard->name)">
+                                :active="route_is(Routes::dashboard)">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
@@ -82,7 +82,7 @@ use App\Http\Routes;
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route_as(Routes::dashboard)"
-                                   :active="request()->routeIs(Routes::dashboard->name)">
+                                   :active="route_is(Routes::dashboard)">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>

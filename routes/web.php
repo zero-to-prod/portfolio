a7672ccb1dd157984121738a39388c0f65f4721e
David Smith <dasmith@zero-to-prod.com>
2023-02-26 12:34:35 -0500

    Remove caching for Connect page
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use App\Http\Views;
 use Illuminate\Support\Facades\Route;
 
 Route::getAs(Routes::cv, Views::cv, cached: true);
-Route::getAs(Routes::connect, Views::connect, cached: true);
+Route::getAs(Routes::connect, Views::connect);
 Route::postAs(Routes::connect_store, ConnectStoreController::class);
 Route::getAs(Routes::welcome, Views::welcome, cached: true);

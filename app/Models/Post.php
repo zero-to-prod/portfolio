f164d3e15e6a8b08eefce490f16a69a2795b2931
David Smith <dasmith@zero-to-prod.com>
2023-02-27 18:12:39 -0500

    Add Posts
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Support\HasRules;
+use App\Models\Support\IdColumn;
+use App\Models\Support\PostColumns;
+use App\Models\Support\PostRules;
+use App\Models\Support\SlugColumn;
+use App\Models\Support\SoftDeleteColumn;
+use App\Models\Support\TimeStampColumns;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @mixin IdeHelperPost
+ */
+class Post extends Model implements HasRules
+{
+    use HasFactory;
+    use IdColumn;
+    use TimeStampColumns;
+    use SoftDeletes;
+    use SoftDeleteColumn;
+    use SlugColumn;
+    use PostColumns;
+    use PostRules;
+
+    protected $fillable = [self::title, self::body];
+}

de1f169b818352f3b80a67d95bb733e67d3f9d5c
David Smith <dasmith@zero-to-prod.com>
2023-02-27 19:55:08 -0500

    Add has slug to SlugColumn
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- app/Models/Support/SlugColumn.php
+++ app/Models/Support/SlugColumn.php
@@ -2,7 +2,22 @@
 
 namespace App\Models\Support;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
 trait SlugColumn
 {
+    use HasSlug;
+
     public const slug = 'slug';
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom(self::name)
+            ->saveSlugsTo('slug');
+    }
 }

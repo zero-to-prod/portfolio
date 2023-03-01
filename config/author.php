07e3a343c63376b43cd8433a763b35b6b1fca74b
David Smith <dasmith@zero-to-prod.com>
2023-03-01 12:25:58 -0500

    Refactor migrations.

--- config/author.php
+++ config/author.php
@@ -5,7 +5,8 @@ use App\Models\Author;
 
 return [
     'default' => [
+        Author::slug => 'david',
         Author::name => 'David Smith',
-        Author::slug => 'david'
+        Author::title => 'Contributor',
     ]
 ];

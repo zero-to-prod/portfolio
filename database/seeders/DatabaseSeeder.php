3a40d93ddcb2713f133acab58caf69f160265bc1
David Smith <dasmith@zero-to-prod.com>
2023-03-01 11:29:52 -0500

    Add post seeder.
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             AuthorSeeder::class,
+            PostSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Author;
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $markdown = <<<'MARKDOWN'
+# h1 Heading
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+
+## Code
+
+Inline `code`
+
+Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+
+Block code "fences"
+
+```
+Sample text here...
+```
+
+Syntax highlighting
+
+``` js
+var foo = function (bar) {
+  return bar++;
+};
+
+console.log(foo(5));
+```
+MARKDOWN;
+
+        $post = Post::create([
+            Post::title => 'First',
+            Post::body => $markdown,
+        ]);
+
+        $post->publish();
+
+        $post->authors()->attach(Author::first());
+        $post->attachTag('tag');
+    }
+}

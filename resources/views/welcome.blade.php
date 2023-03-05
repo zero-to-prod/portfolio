07768c42fa0dfd11e439d1ba0703c46f7aa0647a
David Smith <dasmith@zero-to-prod.com>
2023-03-04 20:55:45 -0500

    Refactor welcome.blade.php
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,7 @@ $tags = Tag::mostViewed()
 ?>
 
 <x-main>
-    <nav class="fixed top-0 bottom-0 left-0 hidden pl-4 mt-[64px] min-[780px]:w-[64px] min-[1312px]:w-[240px] min-[780px]:block"
-         id="drawer"
-    >
+    <nav class="fixed top-0 bottom-0 left-0 hidden pl-4 mt-[64px] min-[780px]:w-[64px] min-[1312px]:w-[240px] min-[780px]:block">
         <h3 class="font-bold text-gray-800">Explore</h3>
         <ul>
             @forEach($tags as $tag)
@@ -20,16 +18,16 @@ $tags = Tag::mostViewed()
             @endforeach
         </ul>
     </nav>
-    <div class="ml-0 flex bg-white min-[780px]:ml-[64px] min-[1312px]:ml-[238px]">
-        <div class="flex w-full flex-col max-w-[2535px] min-[2535px]:mx-auto 2col:pt-6 2col:gap-6">
+    <div class="ml-0 2col:px-4 min-[780px]:ml-[64px] min-[1312px]:ml-[238px]">
+        <div class="flex w-full flex-col max-w-[2535px] min-[2535px]:mx-auto">
             @foreach($tags as $tag)
-                <section class="overflow-hidden 2col:px-4">
-                    <h2 class="mb-4 hidden text-lg font-bold 2col:block">{{$tag->name}}</h2>
-                    <div class="grid grid-flow-row grid-cols-1 2col:grid-cols-2 3col:grid-cols-3 4col:grid-cols-4 5col:grid-cols-5 6col:grid-cols-6 2col:gap-4">
+                <section>
+                    <h2 class="mb-4 2col:block hidden text-lg font-bold">{{$tag->name}}</h2>
+                    <div class="grid grid-flow-row 2col:grid-cols-2 3col:grid-cols-3 4col:grid-cols-4 5col:grid-cols-5 6col:grid-cols-6 2col:gap-4">
                         @foreach($tag->recommended()->take(4) as $post)
-                            <a href="{{route_as(Routes::read, $post)}}" class="flex flex-col">
+                            <a href="{{route_as(Routes::read, $post)}}">
                                 <div class="relative">
-                                    <div class="overflow-hidden bg-gray-200 aspect-w-3 aspect-h-2 group-hover:opacity-75 2col:rounded-lg">
+                                    <div class="overflow-hidden 2col:rounded-lg aspect-w-3 aspect-h-2">
                                         <img class="h-full w-full object-cover object-center"
                                              src="{{ route_as(Routes::file, ['file' => $post->featuredImage()->name, 'width' => 300])}}"
                                              alt="{{$post->featuredImage()->original_name}}"
@@ -41,18 +39,18 @@ $tags = Tag::mostViewed()
                                         {{$post->reading_time . ' min'}}
                                     </div>
                                     @if($post->originally_published_at->between(now(), now()->subDays(1)))
-                                        <div class="absolute bottom-0 m-2 rounded bg-sky-600 px-1 text-xs text-white shadow right-left">
+                                        <div class="absolute bottom-0 m-2 rounded bg-sky-600 px-1 text-xs text-white shadow">
                                             new
                                         </div>
                                     @endif
                                 </div>
-                                <div class="bg-white p-3 2col:px-0">
-                                    <h3 class="font-bold tracking-tight font-sm break-word"
+                                <div class="p-3 2col:px-0">
+                                    <h3 class="font-bold font-sm break-word"
                                         title="{{ $post->title }}">{{ $post->title }}</h3>
                                     <div>
-                                        <p class="text-sm tracking-tight text-gray-600"
+                                        <p class="text-sm text-gray-600"
                                            title="{{$post->authorList()}}">{{$post->authorList()}}</p>
-                                        <p class="text-xs text-sm tracking-tight text-gray-600">{{$post->views_count}} {{$post->views_count === 1 ? 'view' : 'views'}}
+                                        <p class="text-xs text-sm text-gray-600">{{$post->views_count}} {{$post->views_count === 1 ? 'view' : 'views'}}
                                             <span before="•"
                                                   class="before:content-[attr(before)]"> {{$post->published_at->diffForHumans()}}</span>
                                         </p>

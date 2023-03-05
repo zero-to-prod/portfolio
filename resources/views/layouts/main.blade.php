0295e3b04867caea959cba3807cf3ae12e532da3
David Smith <dasmith@zero-to-prod.com>
2023-03-04 20:30:28 -0500

    Refactor main.blade.php
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,71 +19,51 @@ $search = SearchController::search;
 </head>
 <body class="min-h-screen overflow-y-scroll">
 <main aria-label="Main">
-    <div id="content">
-        <header class="fixed top-0 z-50 w-full 2col:overflow-y-visible 2col:shadow shadow-lg h-[56px] min-[780px]:shadow-none">
-            <div class="mx-auto pr-4 bg-white">
-                <div class="relative flex justify-between gap-2">
-                    <a class="flex pl-4 text-lg" href="{{route_as(Routes::welcome)}}">
-                        <div class="my-auto"><span class="font-semibold text-sky-600">dev</span>READ</div>
-                    </a>
-                    <div class="w-full py-2">
-                        <form class="max-w-2xl mx-auto" action="{{route_as(Routes::search)}}" method="post">
-                            @csrf
-                            <label for="{{$search}}"
-                                   class="block text-sm font-medium leading-6 text-gray-900"></label>
-                            <div class="flex rounded-md shadow-sm">
-                                <div class="relative flex flex-grow items-stretch focus-within:z-10">
-                                    <input class="block w-full rounded-none rounded-l-md border-0 pl-4 placeholder:text-gray-400 text-gray-900 ring-1 ring-inset ring-gray-300 h-[40px] focus:ring-1 focus:ring-inset focus:ring-sky-600"
-                                           name="{{$search}}"
-                                           id="{{$search}}"
-                                           value="{{request()->query(ResultsController::query)}}"
-                                           placeholder="Search">
-                                </div>
-                                <button class="relative -ml-px inline-flex items-center rounded-r-md bg-gray-100 p-2 2col:px-4 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 gap-x-1.5 hover:bg-gray-200">
-                                    <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                                         aria-hidden="true">
-                                        <path fill-rule="evenodd"
-                                              d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                                              clip-rule="evenodd"/>
-                                    </svg>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="hidden 2col:block my-auto">About</div>
-                    <div class="flex 2col:hidden items-center">
-                        <!-- Mobile menu button -->
-                        <button type="button"
-                                class="-mx-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500"
-                                aria-expanded="false">
-                            <span class="sr-only">Open menu</span>
-                            <!--
-                              Icon when menu is closed.
-
-                              Menu open: "hidden", Menu closed: "block"
-                            -->
-                            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-                            </svg>
-                            <!--
-                              Icon when menu is open.
-
-                              Menu open: "block", Menu closed: "hidden"
-                            -->
-                            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+    <header class="fixed top-0 z-50 mx-auto w-full bg-white">
+        <div class="flex justify-between">
+            <a class="flex p-4 text-lg" href="{{route_as(Routes::welcome)}}">
+                <span class="font-semibold text-sky-600">dev</span>READ
+            </a>
+            <div class="mx-auto w-full max-w-2xl py-2">
+                <form action="{{route_as(Routes::search)}}" method="post">
+                    @csrf
+                    <label for="{{$search}}"></label>
+                    <div class="flex rounded-md">
+                        <div class="relative flex flex-grow focus-within:z-10">
+                            <input class="w-full rounded-l-md pl-4 placeholder:text-gray-400 text-gray-900 ring-1 ring-inset ring-gray-400 h-[40px]"
+                                   name="{{$search}}"
+                                   id="{{$search}}"
+                                   value="{{request()->query(ResultsController::query)}}"
+                                   placeholder="Search">
+                        </div>
+                        <button class="-ml-px rounded-r-md bg-gray-100 p-2 2col:px-4 ring-1 ring-inset ring-gray-400 hover:bg-gray-200">
+                            <svg class="h-5 w-5 text-gray-600" viewBox="0 0 20 20" fill="currentColor"
+                                 aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                      d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                                      clip-rule="evenodd"/>
                             </svg>
                         </button>
                     </div>
-                </div>
+                </form>
             </div>
-        </header>
-        <div class="mt-[64px]">
-            {{$slot}}
+            <div class="2col:block hidden p-4">About</div>
+            <div class="flex 2col:hidden items-center px-4">
+                <button type="button"
+                        class="-mx-2 inline-flex items-center justify-center rounded-md p-2 text-gray-500 hover:bg-gray-200 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500"
+                        aria-expanded="false">
+                    <span class="sr-only">Open menu</span>
+                    <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                    </svg>
+                </button>
+            </div>
         </div>
+    </header>
+    <div class="mt-[64px]">
+        {{$slot}}
     </div>
 </main>
 </body>

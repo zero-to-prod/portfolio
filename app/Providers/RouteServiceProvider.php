c1913c7db10790a3b76a0cdff995b4ee64320f70
David Smith <dasmith@zero-to-prod.com>
2023-03-09 14:09:23 -0500

    https for routes
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -25,6 +25,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (App::environment('production')) {
+            URL::forceSchema('https');
+        }
+
         $this->configureRateLimiting();
 
         $this->routes(function () {

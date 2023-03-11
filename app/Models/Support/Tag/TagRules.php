e1e54111aa72c48fe84cdb928fcc3103b0634dde
David Smith <dasmith@zero-to-prod.com>
2023-03-10 20:25:01 -0500

    Update Tag rules
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- app/Models/Support/Tag/TagRules.php
+++ app/Models/Support/Tag/TagRules.php
@@ -7,7 +7,7 @@ trait TagRules
     public static function rules(string $column): array
     {
         return match ($column) {
-            self::name => ['required', 'max:10'],
+            self::name => ['required', 'max:15', 'unique:tags,name'],
         };
     }
 }

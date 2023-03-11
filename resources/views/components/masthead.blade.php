ea3c330ef86aaed650524940965931ca0b6b364c
David Smith <dasmith@zero-to-prod.com>
2023-03-11 16:54:21 -0500

    Update post.css
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- resources/views/components/masthead.blade.php
+++ resources/views/components/masthead.blade.php
@@ -18,8 +18,8 @@ $search = SearchRedirect::search;
             <x-svg :name="'hamburger'" class="m-auto block h-6 w-[64px]"/>
         </button>
         <x-a class="mr-2 flex px-2 py-4 text-lg 2col:text-xl btn-ghost" :href="R::welcome()">
-            <span class="rounded-l pr-1 pl-2 font-semibold text-rose-500 shadow">dev</span><span
-                        class="rounded-r bg-rose-500 pr-2 pl-1 font-semibold text-white shadow-md">READ</span>
+            <span class="rounded-l pr-1 pl-2 font-semibold bg-white text-sky-600 shadow">dev</span><span
+                        class="rounded-r bg-sky-600 pr-2 pl-1 font-semibold text-white shadow-md">READ</span>
         </x-a>
         <div class="mx-auto my-auto w-full max-w-2xl 2col:pr-0 pr-2">
             <form action="{{R::search()}}" method="post">

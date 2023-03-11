999285b9c0b39a0bda8a1b6f3b825908b3c68d1a
David Smith <dasmith@zero-to-prod.com>
2023-03-10 19:55:41 -0500

    Update UI
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,21 +9,21 @@ use App\Models\Tag;
 ?>
 
 <x-main>
-    <div class="flex w-full flex-col 4col:mx-auto 4col:max-w-7xl">
+    <div class="4col:mx-auto flex w-full 4col:max-w-7xl flex-col">
         @foreach($tags as $tag)
             <section>
-                <x-a class="p-2 2col:mb-0 flex gap-x-2 2col:ml-0 btn-ghost" :href="R::results($tag)">
+                <x-a class="2col:mb-0 2col:ml-0 flex gap-x-2 p-2 btn-ghost" :href="R::results($tag)">
                     @if($tag->hasLogo())
-                        <x-img class="w-10 my-auto rounded" :file="$tag->logo()" :width="80"/>
+                        <x-img class="my-auto w-10 rounded" :file="$tag->logo()" :width="80"/>
                     @endif
                     <h2 class="my-auto text-lg font-semibold">
                         {{$tag->name}}
                     </h2>
                 </x-a>
-                <x-divider class="mb-4 mt-2"/>
-                <div class="grid mb-4 grid-flow-row 2col:grid-cols-2 4col:grid-cols-4 2col:gap-2">
+                <x-divider class="mt-2 mb-4"/>
+                <div class="mb-4 grid grid-flow-row 2col:grid-cols-2 4col:grid-cols-4 2cols:gap-0 2col:gap-2 gap-4">
                     @foreach($tag->relatedPosts(limit: 4) as $post)
-                        <x-a :href="R::read($post)">
+                        <x-a class="shadow-lg 2col:shadow-none" :href="R::read($post)">
                             <div class="relative">
                                 <div class="overflow-hidden 2col:rounded-lg aspect-w-3 aspect-h-2">
                                     <x-img class="h-full w-full object-cover object-center"
@@ -36,12 +36,12 @@ use App\Models\Tag;
                                 <x-new-chip :post="$post"/>
                             </div>
                             <div class="p-2 2col:px-0 2col:pt-1">
-                                <h3 class="font-bold font-sm break-word tracking-tight leading-5"
+                                <h3 class="font-bold leading-5 tracking-tight font-sm break-word"
                                     title="{{ $post->title }}">{{ $post->title }}</h3>
-                                <p class="text-sm 2col:hidden" title="{{$post->subtitle}}">
+                                <p class="2col:hidden text-sm" title="{{$post->subtitle}}">
                                     {{$post->subtitle}}
                                 </p>
-                                <p class="text-sm mt-2 2col:mt-0"
+                                <p class="2col:mt-0 mt-2 text-sm"
                                    title="{{$post->authorList()}}">
                                     {{$post->authorList()}}
                                 </p>

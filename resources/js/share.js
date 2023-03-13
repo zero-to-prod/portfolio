ef0a0a945a760b2cbab0bf355e289c98459bb8f4
David Smith <dasmith@zero-to-prod.com>
2023-03-13 08:53:01 -0400

    Update share
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- resources/js/share.js
+++ resources/js/share.js
@@ -4,22 +4,15 @@ const title = document.getElementById('title')
 const subtitle = document.getElementById('subtitle')
 const url = window.url
 
-// Attach the submit event handler
-button.addEventListener('click', shareFiles)
-
-// Share files on submit
-async function shareFiles() {
-    const userAgentData = await navigator.userAgentData.getHighEntropyValues(['platform', 'platformVersion']);
-    const data = {
-        title: title.innerText,
-        text: subtitle.innerText,
-        url: url,
+button.addEventListener('click', event => {
+    if (navigator.share) {
+        navigator.share({
+            title: title.innerText,
+            text: subtitle.innerText,
+            url: url,
+        }).then(() => {
+            console.log('Thanks for sharing!');
+        })
+            .catch(console.error);
     }
-
-    if (userAgentData.platform === 'iOS' && parseFloat(userAgentData.platformVersion) < 14.7) {
-        // Handle iOS versions that don't support the Web Share API
-        // Redirect to a share page or display a share sheet overlay
-    } else {
-        await navigator.share(data);
-    }
-}
+});

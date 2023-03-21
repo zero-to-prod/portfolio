8f7856b52a23fdf8a2e8fed1426f342e61b3e93c
David Smith <dasmith@zero-to-prod.com>
2023-03-21 18:26:33 -0400

    Add subscription form
    Signed-off-by: David Smith <dasmith@zero-to-prod.com>

--- app/Http/Livewire/Subform.php
+++ app/Http/Livewire/Subform.php
@@ -2,13 +2,80 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Contact;
+use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Validation\ValidationException;
 use Livewire\Component;
+use Stripe\StripeClient;
 
 class Subform extends Component
 {
+    public const plan = 'plan';
+    public const email = 'email';
+    public const card = 'card';
+    public const month = 'month';
+    public const year = 'year';
+    public const cvc = 'cvc';
+
+    public $plan = 'year';
+    public $email;
+    public $card;
+    public $month = '';
+    public $year = '';
+    public $cvc;
+
+    public function rules(): array
+    {
+        return [
+            self::email => Contact::rules(Contact::email),
+            self::card => 'required|numeric|digits_between:13,19',
+            self::month => 'required',
+            self::year => 'required',
+            self::cvc => 'required|numeric|digits_between:3,4'
+        ];
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function updated($propertyName): void
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit(): void
+    {
+        $this->validate();
+        try {
+
+            $stripe = app()->make(StripeClient::class);
+
+            $customer = $stripe->customers->create([self::email => $this->email]);
+            $payment_method = $stripe->paymentMethods->create([
+                'type' => 'card',
+                'card' => [
+                    'number' => $this->card,
+                    'exp_month' => $this->month,
+                    'exp_year' => $this->year,
+                    'cvc' => $this->cvc,
+                ],
+            ]);
+            $stripe->paymentMethods->attach($payment_method->id, ['customer' => $customer->id]);
+            $stripe->subscriptions->create([
+                'customer' => $customer->id,
+                'items' => [
+                    ['price' => $this->plan === 'month' ? config('stripe.monthly_plan') : config('stripe.yearly_plan')],
+                ],
+                'default_payment_method' => $payment_method->id,
+            ]);
+        } catch (Exception $e) {
+            $this->addError('stripe', $e->getMessage());
+        }
+    }
+
     public function render(): Factory|View|Application
     {
         return view('livewire.subform');
--- resources/views/livewire/subform.blade.php
+++ resources/views/livewire/subform.blade.php
@@ -1,12 +1,12 @@
-<div x-data="{ selected: 'year' }" id="subscribe">
+<div x-data="{ plan: @entangle('plan') }" id="subscribe">
     <div class="mx-4 mx-auto mt-2 2col:mt-6 flex flex-wrap justify-center gap-4">
-        <div class="card" @click="selected='month'" :class="selected === 'month' ? 'active' : ''">
+        <div class="card" @click="plan='month'" :class="plan === 'month' ? 'active' : ''">
             <div class="flex justify-between">
                 <div>
                     <h2>Monthly</h2>
                     <h3>$6/month</h3>
                 </div>
-                <x-svg style="display: none" x-show="selected === 'month'" :name="'check-green'"/>
+                <x-svg style="display: none" x-show="plan === 'month'" :name="'check-green'"/>
             </div>
             <ul>
                 <li>
@@ -31,13 +31,13 @@
                 </li>
             </ul>
         </div>
-        <div @click="selected='year'" class="card" :class="selected === 'year' ? 'active' : ''">
+        <div @click="plan='year'" class="card" :class="plan === 'year' ? 'active' : ''">
             <div class="flex justify-between">
                 <div>
                     <h2>Yearly</h2>
                     <h3>$60/year ($5/month)</h3>
                 </div>
-                <x-svg style="display: none" x-show="selected === 'year'" :name="'check-green'"/>
+                <x-svg style="display: none" x-show="plan === 'year'" :name="'check-green'"/>
             </div>
             <ul>
                 <li>
@@ -62,13 +62,13 @@
                 </li>
             </ul>
         </div>
-        <div @click="selected='member'" :class="selected === 'member' ? 'active' : ''" class="card">
+        <div @click="plan='member'" :class="plan === 'member' ? 'active' : ''" class="card">
             <div class="flex justify-between">
                 <div>
                     <h2>Member</h2>
                     <h3>Free - with login</h3>
                 </div>
-                <x-svg style="display: none" x-show="selected === 'member'" :name="'check-green'"/>
+                <x-svg style="display: none" x-show="plan === 'member'" :name="'check-green'"/>
             </div>
             <ul>
                 <li>
@@ -89,13 +89,13 @@
                 </li>
             </ul>
         </div>
-        <div @click="selected='none'" :class="selected === 'none' ? 'active' : ''" class="card">
+        <div @click="plan='none'" :class="plan === 'none' ? 'active' : ''" class="card">
             <div class="flex justify-between">
                 <div>
                     <h2>None</h2>
                     <h3>Free - no email required</h3>
                 </div>
-                <x-svg style="display: none" x-show="selected === 'none'" :name="'check-green'"/>
+                <x-svg style="display: none" x-show="plan === 'none'" :name="'check-green'"/>
             </div>
             <ul>
                 <li>
@@ -119,73 +119,119 @@
     </div>
     <x-divider class="py-8 !block "/>
     <div class="mx-auto max-w-[380px] min-h-[300px] ">
-        <div x-show="selected === 'member'" style="display: none">
+        <div x-show="plan === 'member'" style="display: none">
             <x-a :href="to()->register->index()"
                  class="flex rounded-lg border-2 border border-gray-300 border-sky-600 p-2 font-bold hover:shadow-lg">
                 <span class="mx-auto">Continue to Register for Free</span>
             </x-a>
         </div>
-        <div x-show="selected === 'none'" style="display: none">
+        <div x-show="plan === 'none'" style="display: none">
             <x-a :href="to()->welcome()"
                  class="flex rounded-lg border-2 border border-gray-300 border-sky-600 p-2 font-bold hover:shadow-lg">
                 <span class="mx-auto">Continue to Without Paying</span>
             </x-a>
         </div>
-        <div id="form-wrapper" x-show="selected === 'year' || selected === 'month'" x-transition.opacity>
-            <form id="form" class="space-y-4">
+        <div id="form-wrapper" x-show="plan === 'year' || plan === 'month'" x-transition.opacity>
+            <form wire:submit.prevent="submit" class="space-y-4">
+                <input wire:model="plan" type="hidden" name="plan"/>
                 <div>
                     <label class="text-sm font-bold" for="email">Email</label>
-                    <input class="text-lg input"
+                    @error('email') <p class="text-sm font-bold text-error">{{ $message }}</p> @enderror
+                    <input wire:model.lazy="email"
+                           @class([
+                                 'text-lg input',
+                                 'ring-red-500' => $errors->has('email'),
+                                 ])
                            id="email"
                            type="email"
                            name="email"
                            value="{{old('email')}}"
-                           placeholder="Your email address"
                            required
+                           placeholder="Your email address"
                     />
-                    <p id="email-errors" class="text-sm font-bold text-error"></p>
                 </div>
                 <div>
                     <p class="text-sm font-bold">Card Information</p>
-                    <label for="card_number" class="sr-only">Card number</label>
-                    <input class="rounded-br-none rounded-bl-none text-lg input tracking-[.2rem]"
+                    @error('card') <span class="text-sm font-bold text-error">{{ $message }}</span> @enderror
+                    <label for="card" class="sr-only">Card number</label>
+                    <input wire:model.lazy="card"
+                           @class([
+                              'input text-lg rounded-none rounded-t',
+                              'ring-red-500' => $errors->has('card'),
+                          ])
                            autocomplete="cc-number"
-                           autocorrect="off"
                            spellcheck="false"
-                           id="card_number"
-                           name="card_number"
+                           id="card"
+                           name="card"
                            type="text"
                            inputmode="numeric"
                            aria-label="Card number"
                            placeholder="1234 1234 1234 1234"
                            aria-invalid="false"
                            value=""
+                           required
                            minlength="13"
                            maxlength="19"
-                           required
-                    >
+                    />
                     <div class="flex">
-                        <label for="$expiration" class="sr-only">Expiration Month</label>
-                        <input class="rounded-none rounded-bl text-lg input -mt-[1px] -mr-[.5px]"
-                               autocomplete="cc-exp"
-                               autocorrect="off"
-                               spellcheck="false"
-                               id="$expiration"
-                               name="$expiration"
-                               type="text"
-                               inputmode="numeric"
-                               aria-label="Expiration"
-                               placeholder="MM / YY"
-                               aria-invalid="false"
-                               value=""
-                               maxlength="7"
-                               minlength="7"
-                               required
+                        <label for="month" class="sr-only">Expiration Month</label>
+                        <select wire:model="month"
+                                @class([
+                                    'rounded-none rounded-bl text-lg input -mr-[.5px]',
+                                    'ring-red-500' => $errors->has('month'),
+                                ])
+                                autocomplete="cc-exp-month"
+                                spellcheck="false"
+                                id="month"
+                                name="month"
+                                type="text"
+                                required
+                                inputmode="numeric"
+                                aria-label="Expiration Month"
+                                aria-invalid="false"
                         >
+                            <option value="" selected disabled>MM</option>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                        <label for="year" class="sr-only">Expiration Year</label>
+                        <select wire:model="year"
+                                @class([
+                                    'rounded-none rounded-bl text-lg input -mr-[.5px]',
+                                    'ring-red-500' => $errors->has('year'),
+                                ])
+                                autocomplete="cc-exp-year"
+                                spellcheck="false"
+                                id="year"
+                                name="year"
+                                type="text"
+                                required
+                                inputmode="numeric"
+                                aria-label="Expiration Year"
+                                aria-invalid="false"
+                        >
+                            <option value="" selected disabled>YY</option>
+                            @for($i = 0; $i < 12; $i++)
+                                <option value="{{date('y') + $i}}">{{date('y') + $i}}</option>
+                            @endfor
+                        </select>
                         <label for="cvc" class="sr-only">CVC</label>
-                        <input class="rounded-none rounded-br text-lg input -mt-[1px] -ml-[.5px]"
+                        <input wire:model="cvc"
+                               @class([
+                                   'rounded-none rounded-br text-lg input -ml-[.5px]',
+                                   'ring-red-500' => $errors->has('cvc'),
+                               ])
                                autocomplete="cc-csc"
-                               autocorrect="off"
                                spellcheck="false"
                                id="cvc"
                                name="cvc"
@@ -195,26 +241,19 @@
                                placeholder="CVC"
                                aria-invalid="false"
                                value=""
+                               required
                                minlength="3"
                                maxlength="4"
-                               required
                         >
                     </div>
                     <div class="text-sm font-bold text-error">
-                        <p id="expiration-error"></p>
-                        <p id="card-errors"></p>
-                        <p id="cvc-errors"></p>
-                        <p id="expiration-errors"></p>
-                        <p id="message-errors"></p>
+                        @error('month') <p>{{ $message }}</p> @enderror
+                        @error('year') <p>{{ $message }}</p> @enderror
+                        @error('cvc') <p>{{ $message }}</p> @enderror
+                        @error('stripe') <p>{{ $message }}</p> @enderror
                     </div>
                 </div>
-                <button id="submit" class="font-bold btn btn-wide">Subscribe</button>
-                <div id="message-success" class="hidden text-sm font-bold">
-                    <p class="flex gap-1">
-                        <x-svg :name="'check-green'"/>
-                        <span class="my-auto">Thank you for supporting! A receipt will be sent to your email</span>
-                    </p>
-                </div>
+                <button class="font-bold btn btn-wide">Subscribe</button>
             </form>
         </div>
     </div>
